679e4477ff88a3d29fa95be2c073b3a22cd8660f
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-03-23 19:54:07 +0500

    add react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from "react";
-import { Container, Carousel } from 'react-bootstrap';
-import SearchForm from "./components/SearchForm";
-import DropdownMenu from "./components/Dropdown";
-import Results from "./components/Results";
+import { Col, Row, Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+
 
 function App() {
   return (
     <div>
-      <Carousel >
-        <Container>
-          <h1>Search for Books</h1>
-          <SearchForm />
-          <hr></hr>
-          <p>Categories:</p>
-          <DropdownMenu />
-          <br></br>
-          <p>Sorting by:</p>
-          <DropdownMenu />
-        </Container>
-      </Carousel>
-      <hr></hr>
-      <Results />
+      <Container>
+        <Row>
+          <Col sm={2}></Col>
+          <Col sm={8}>
+            <h1>Search book app</h1>
+          </Col>
+          <Col sm={2}></Col>
+        </Row>
+        <Row>
+          <Col sm={2}></Col>
+          <Col sm={8}>
+            <InputGroup className="mb-3 mt-3 w-60 mx-auto">
+              <Form.Control
+                placeholder="Enter your search..."
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <Button variant="secondary" id="button-addon2">
+                Start
+              </Button>
+            </InputGroup>
+          </Col>
+          <Col sm={2}></Col>
+        </Row>
+        <Row>
+          <Col sm={3}></Col>
+          <Col sm={3}>
+            <h6>Category:</h6>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+          <Col sm={3}>
+            <h6>Filter:</h6>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Dropdown Button
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+          <Col sm={3}></Col>
+        </Row>
+      </Container>
+
     </div>
   );
 }
