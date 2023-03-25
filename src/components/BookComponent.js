f6bed61489075a3a17bc5a98b822952813e16102
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-03-25 13:23:18 +0500

    Raw books container layout

--- src/components/BookComponent.js
+++ src/components/BookComponent.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 const BookComponent = (props) => {
     //let url = `https://live.staticflickr.com/${props.serverId}/${props.nameId}_${props.secret}_w.jpg`;
-    //console.log(url);
-    // return (
-    //   <li>
-    //     <img src={url} alt={props.title} />
-    //   </li>
-    // );
+    console.log(props.author);
+    return (
+      <li>
+        <h4><strong>{props.title}</strong></h4>
+        <h6>{props.category}</h6>
+        <h6>{props.author}</h6>
+        <img src={props.thumbnail} />
+      </li>
+    );
 }
 
 export default BookComponent;
--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -3,26 +3,26 @@ import BookComponent from './BookComponent';
 
 const BookContainer = (props) => {
     const results = props.data;
-    //let photos;
+    let books;
     console.log(results);
-    if (results.length > 0) {
-        //photos = results.map(photo => <BookComponent serverId={photo.server} nameId={photo.id} secret={photo.secret} key={photo.id} title={photo.title}/>)
-    } else {
+    // if (results.length > 0) {
+    books = results.map(book => <BookComponent title={book.volumeInfo.title} category={book.volumeInfo.categories} author={book.volumeInfo.authors} thumbnail={book.volumeInfo.imageLinks.thumbnail} />);
+    // } else {
         //photos = <NotFound />;
-    }
-    // return (
-    //     <div className="book-container">
-    //         <h2>Results</h2>
-    //         <ul>
-    //             {photos}
-    //             {/* <PhotoComponent src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg"/>
-    //             <PhotoComponent src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg"/>
-    //             <PhotoComponent src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg"/>
-    //             <PhotoComponent src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg"/>
-    //             <NotFound /> */}
-    //         </ul>
-    //     </div>
-    // );
+    // }
+    return (
+        <div className="book-container">
+            <h2>Results</h2>
+            <ol>
+                {books}
+                {/* <PhotoComponent src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg"/>
+                <PhotoComponent src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg"/>
+                <PhotoComponent src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg"/>
+                <PhotoComponent src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg"/>
+                <NotFound /> */}
+            </ol>
+        </div>
+    );
 }
 
 export default BookContainer;
